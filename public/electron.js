5c533c3e1578721eb5a4ea340ea72830eff32ffc
vbhv98 <vaibhavbhawalkar98.vb@gmail.com>
2020-03-18 02:27:19 +0530

    Blonk Detection Integrated

--- public/electron.js
+++ public/electron.js
@@ -1,96 +1,96 @@
-const electron = require('electron');
-const path = require('path');
-const url = require('url');
+const electron = require('electron')
+const path = require('path')
+const url = require('url')
+const { ipcMain } = require('electron')
 
-const { app } = electron;
-const { BrowserWindow } = electron;
+const { app } = electron
+const { BrowserWindow } = electron
 
-const { ipcMain } = require('electron');
+let mainWindow
 
-let mainWindow;
+app.allowRendererProcessReuse = true
 
 function createWindow() {
-	const startUrl = process.env.DEV
-		? 'http://localhost:3000'
-		: url.format({
-				pathname: path.join(__dirname, '/../build/index.html'),
-				protocol: 'file:',
-				slashes: true,
-		  });
-	mainWindow = new BrowserWindow({
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	});
+  const startUrl = process.env.DEV
+    ? 'http://localhost:3000'
+    : url.format({
+        pathname: path.join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+      })
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-	mainWindow.loadURL(startUrl);
-	process.env.DEV && mainWindow.webContents.openDevTools();
+  mainWindow.loadURL(startUrl)
+  // process.env.DEV && mainWindow.webContents.openDevTools();
 
-	mainWindow.on('closed', function() {
-		mainWindow = null;
-	});
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
 }
 
-app.on('ready', createWindow);
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('activate', () => {
-	if (mainWindow === null) {
-		createWindow();
-	}
-});
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
 
 // ------------------- set up event listeners here --------------------
 
 // temporary variable to store data while background
 // process is ready to start processing
 let cache = {
-	data: undefined,
-};
+  data: undefined
+}
 
 // a window object outside the function scope prevents
 // the object from being garbage collected
-let hiddenWindow;
+let hiddenWindow
+
+ipcMain.on('STOP_HIDDEN', () => {
+  hiddenWindow.close()
+  console.log('closed')
+})
 
 // This event listener will listen for request
 // from visible renderer process
 ipcMain.on('START_BACKGROUND_VIA_MAIN', (event, args) => {
-	const backgroundFileUrl = url.format({
-		pathname: path.join(__dirname, `../background_tasks/background.html`),
-		protocol: 'file:',
-		slashes: true,
-	});
-	hiddenWindow = new BrowserWindow({
-		show: false,
-		webPreferences: {
-			nodeIntegration: true,
-		},
-	});
-	hiddenWindow.loadURL(backgroundFileUrl);
+  const backgroundFileUrl = url.format({
+    pathname: path.join(__dirname, `../background_tasks/background.html`),
+    protocol: 'file:',
+    slashes: true
+  })
+  hiddenWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  hiddenWindow.loadURL(backgroundFileUrl)
 
-	hiddenWindow.webContents.openDevTools();
+  hiddenWindow.on('closed', () => {
+    hiddenWindow = null
+  })
 
-	hiddenWindow.on('closed', () => {
-		hiddenWindow = null;
-	});
-
-	cache.data = args.number;
-});
+  // cache.data = args.message
+})
 
 // This event listener will listen for data being sent back
 // from the background renderer process
 ipcMain.on('MESSAGE_FROM_BACKGROUND', (event, args) => {
-	mainWindow.webContents.send('MESSAGE_FROM_BACKGROUND_VIA_MAIN', args.message);
-});
+  mainWindow.webContents.send('MESSAGE_FROM_BACKGROUND_VIA_MAIN', args)
+})
 
 ipcMain.on('BACKGROUND_READY', (event, args) => {
-
-	event.reply('START_PROCESSING', {
-		data: cache.data,
-	});
-});
+  event.reply('START_PROCESSING')
+})
--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-const electron = window.require('electron');
-const { ipcRenderer } = electron;
+const electron = window.require('electron')
+const { ipcRenderer } = electron
 
-class App extends Component {
-	componentDidMount() {
-		// setting up an event listener to read data that background process
-		// will send via the main process after processing the data we
-		// send from visiable renderer process
-		ipcRenderer.on('MESSAGE_FROM_BACKGROUND_VIA_MAIN', (event, args) => {
-			console.log(args);
-		});
+const App = () => {
+  const [data, setData] = React.useState({ message: 'Loading' })
 
-		// trigger event to start background process
-		// can be triggered pretty much from anywhere after
-		// you have set up a listener to get the information
-		// back from background process, as I have done in line 13
-		ipcRenderer.send('START_BACKGROUND_VIA_MAIN', {
-			number: 25,
-		});
-	}
+  React.useEffect(() => {
+    // setting up an event listener to read data that background process
+    // will send via the main process after processing the data we
+    // send from visiable renderer process
+    ipcRenderer.on('MESSAGE_FROM_BACKGROUND_VIA_MAIN', (event, args) => {
+      setData(args)
+    })
 
-	render() {
-		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-				</header>
-			</div>
-		);
-	}
+    // trigger event to start background process
+    // can be triggered pretty much from anywhere after
+    // you have set up a listener to get the information
+    // back from background process, as I have done in line 13
+  }, [])
+
+  const startBlinkDetection = () => {
+    ipcRenderer.send('START_BACKGROUND_VIA_MAIN')
+  }
+
+  return (
+    <div className='App'>
+      HELLO {JSON.stringify(data)}
+      <button onClick={startBlinkDetection}>Start Detection</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
