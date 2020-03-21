74bf4a2062f5814d57c56e31f3cfa82d4002ede7
vbhv98 <vaibhavbhawalkar98.vb@gmail.com>
2020-03-22 03:36:27 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -3,27 +3,7 @@ import Webcam from 'react-webcam'
 import { DEFAULT_BLINK_LAYOUT } from './blink-layout.json'
 import './App.css'
 
-// const deepCopy = inObject => {
-//   let outObject, value, key
-
-//   if (typeof inObject !== 'object' || inObject === null) {
-//     return inObject // Return the value if inObject is not an object
-//   }
-
-//   // Create an array or object to hold the values
-//   outObject = Array.isArray(inObject) ? [] : {}
-
-//   for (key in inObject) {
-//     value = inObject[key]
-
-//     // Recursively (deep) copy for nested objects, including arrays
-//     outObject[key] = typeof value === 'object' && value !== null ? deepCopy(value) : value
-//   }
-
-//   return outObject
-// }
-
-// const indexMap = [7, 6, 7, 6, 2]
+const indexMap = [7, 6, 7, 6, 2]
 
 const App = () => {
   const [layout, setLayout] = useState(DEFAULT_BLINK_LAYOUT)
@@ -33,25 +13,17 @@ const App = () => {
   return (
     <div className='app'>
       <div className='camera'>
-        <div className='camera-box cool-shadow'>
-          <Webcam height='240px' />
-        </div>
+        <div className='camera-box cool-shadow'>{/* <Webcam height='240px' /> */}</div>
         <div className='text-box cool-shadow'>typed-text</div>
       </div>
       <div className='layout cool-shadow'>
-        {layout.map(({ letterGroup, selection }) => {
-          return (
-            <div className={selection ? 'letter-row  cool-shadow-selected' : 'letter-row '}>
-              {letterGroup.map(({ letter, selection }) => {
-                return (
-                  <div className={selection ? 'letter cool-shadow-selected' : 'letter'}>
-                    {letter}
-                  </div>
-                )
-              })}
-            </div>
-          )
-        })}
+        {layout.map(row => (
+          <div className='letter-row'>
+            {row.map(cell => (
+              <div className='letter'>{cell}</div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   )
